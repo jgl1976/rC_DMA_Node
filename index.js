ed759156c3bd703398a31c6bdff716a3d9748e72
John G. Levis Jr <jlevis@webitso.com>
2015-03-04 10:20:20 -0500

    Learning... BangHeadOnWall

--- index.js
+++ index.js
@@ -13,19 +13,3 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-var jsforce = require('jsforce');
-var conn = new jsforce.Connection({
-  // you can change loginUrl to connect to sandbox or prerelease env.
-  // loginUrl : 'https://test.salesforce.com'
-});
-conn.login(username, password, function(err, userInfo) {
-  if (err) { return console.error(err); }
-  // Now you can get the access token and instance URL information.
-  // Save them to establish connection next time.
-  console.log(conn.accessToken);
-  console.log(conn.instanceUrl);
-  // logged in user property
-  console.log("User ID: " + userInfo.id);
-  console.log("Org ID: " + userInfo.organizationId);
-  // ...
-});
