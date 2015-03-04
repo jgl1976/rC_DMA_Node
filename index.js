6da04d84e47ab68dd89ba666939212182f925367
John G. Levis Jr <jlevis@webitso.com>
2015-03-03 22:41:56 -0500

    Testing Node.js to HTML

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var app = express();
+var http = require('http'),
+    fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello Small Small World! This is a test for roundCorner and Heroku. From Ian, Cid, Don, and John');
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
