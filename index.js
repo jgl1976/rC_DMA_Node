ef117208b7c38143f329abae34ccf780d5beb14e
John G. Levis Jr <jlevis@webitso.com>
2015-03-04 10:18:05 -0500

    reestablish 'hello'

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('jsForce uploaded - this should be a login screen soon....');
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
 var jsforce = require('jsforce');
 var conn = new jsforce.Connection({
   // you can change loginUrl to connect to sandbox or prerelease env.
