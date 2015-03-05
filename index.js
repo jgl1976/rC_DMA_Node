fae6734d5bf926211d0fda3cb9c2d9b46051bb87
Ian Zepp <ian.zepp@gmail.com>
2015-03-05 15:07:29 -0500

    Updated

--- index.js
+++ index.js
@@ -2,29 +2,39 @@ var express = require('express');
 var app = express();
 var jsforce = require('jsforce');
 
+// Config
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
+// Routes
+app.get('/', function (req, res) {
+    var conn = new jsforce.Connection();
 
-	var conn = new jsforce.Connection();
-	conn.login('john.levis+findev@roundcorner.com', 'John_5050', function(err, res) {
-	  if (err) {
-	  	response.send(err);
-	  }//logged in carry on
-	  conn.query('SELECT Id, Name FROM Account WHERE Name = "John Doe"', function(err, res) {
-	    if (err) {
-	     response.send(err); 
-	 	}
-	 	var response = res;
+    conn.login('john.levis+findev@roundcorner.com', 'John_5050', function (errors, result) {
+        if (errors) {
+            console.log('[login] errors', '' + errors)
+            res.status(500);
+            res.send('' + errors);
+            return;
+        } else {
+            console.log('[login] result', result)            
+        }
 
-	  });///end query
-	});
-
-	response.send(response);	  
-
+        // Do stuff
+        conn.query('SELECT Id, Name FROM Account', function (errors, result) {
+            if (errors) {
+                console.log('[query] errors', '' + errors)
+                res.status(500);
+                res.send('' + errors);
+            } else {
+                console.log('[query] result', result)
+                res.status(200);
+                res.send(JSON.stringify(result));
+            }
+        }); ///end query
+    });
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
