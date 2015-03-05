14cfd7d0737115bc813411ac4eba8ee78535c841
John G. Levis Jr <jlevis@webitso.com>
2015-03-05 14:48:05 -0500

    API ConFIG

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var app = express();
+var jsforce = require('jsforce');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello Small Small World!');
+
+	var conn = new jsforce.Connection();
+	conn.login('john.levis+findev@roundcorner.com', 'John_5050', function(err, res) {
+	  if (err) {
+	  	response.send(err);
+	  }//logged in carry on
+	  conn.query('SELECT Id, Name FROM Account WHERE Name = "John Doe"', function(err, res) {
+	    if (err) {
+	     response.send(err); 
+	 	}
+	 	var response = res;
+
+	  });///end query
+	});
+
+	response.send(response);	  
+
 });
 
 app.listen(app.get('port'), function() {
