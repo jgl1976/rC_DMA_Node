e3ecff5f92736c71fde483f788416afb92e95454
John G. Levis Jr <jlevis@webitso.com>
2015-03-05 09:09:14 -0500

    trying to call on external scripts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
+var server = require("server");
+var router = require("router");
+var requestHandlers = require("requestHandlers");
 
 var handle = {};
 handle["/"] = requestHandlers.start;
