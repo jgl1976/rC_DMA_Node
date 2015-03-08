0e2faeb0418994faa1c4f51021a74ce9074ee7ed
Robert Duller <robduller@gmail.com>
2015-03-08 15:01:27 +0000

    set root as public

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var jsforce = require('jsforce');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 
 var accessToken = "";
